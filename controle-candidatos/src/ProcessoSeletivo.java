c1c9d3ddd1afc950b3b26be90417a01c1c5e2b44
bruno <brunogomesdarosavidal@gmail.com>
2024-05-15 13:18:17 -0300

    criação da classe ProcesoSeletivo

---
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -0,0 +1,128 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+
+    public static void main(String[] args) {
+        /*
+         * analisarCandidato(1900.0);
+         * analisarCandidato(2200.0);
+         * analisarCandidato(2000.0);
+         */
+
+        /* selecaoCandidatos(); */
+
+        /* imprimirSelecionados(); */
+
+        String[] candidatos = {
+                "FELIPE",
+                "MARCIA",
+                "JULIA",
+                "PAULO",
+                "AUGUSTO"
+        };
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+
+            atendeu = atender();
+            continuarTentando = !atendeu;
+
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println(
+                    "CONSEGUIMOS CONTATO COM O CANDITATO " + candidato + " NA " + tentativasRealizadas + " TENTATIVA ");
+        else
+            System.out.println("NÂO CONSEGUIMOS CONTATO COM O CANDIDATO " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS "
+                    + tentativasRealizadas);
+    }
+
+    // Metodo auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = {
+                "FELIPE",
+                "MARCIA",
+                "JULIA",
+                "PAULO",
+                "AUGUSTO"
+        };
+
+        System.out.println("Imprimir a lista de candidatos informando o indice do elemento");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de n° " + (indice + 1) + " é o " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+
+        for (String candidato : candidatos) {
+            System.out.println("O candidato selecionado foi : " + candidato);
+        }
+    }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = {
+                "FELIPE",
+                "MARCIA",
+                "JULIA",
+                "PAULO",
+                "AUGUSTO",
+                "MONICA",
+                "FABRICIO",
+                "MIRELA",
+                "DANIELA",
+                "JORGE"
+        };
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salario " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para candidato");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o candidato com contra proposta");
+        } else {
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+    }
+}
